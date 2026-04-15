3f236c5066905ec3aaa2cbe1d11f732f028d9298
Trevon-Woods-AI-Portfolio <fullstackvon@gmail.com>
2026-04-15 10:59:44 -0500

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: "https://sick-or-not-dection.onrender.com",
       methods: ["GET", "POST"]
     },
 });
