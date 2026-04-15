b5edfe513ac23b5757556bb4dec26b8138dd87fb
Trevon-Woods-AI-Portfolio <fullstackvon@gmail.com>
2026-04-15 17:33:00 -0500

    Update Toolbar.jsx

--- frontend/components/Toolbar.jsx
+++ frontend/components/Toolbar.jsx
@@ -17,7 +17,7 @@ const Toolbar = () => {
   const handleLogout = async () => {
     try {
       const res = await fetch("https://health-risk-detector-back.onrender.com/api/user/logout", {
-        method: "POST",
+        method: "GET",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           userData: {
